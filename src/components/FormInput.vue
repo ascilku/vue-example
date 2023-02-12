290c03627c35a6e0baae073bbf4c19294639bd3b
Hajar Aswad <hajaraswadkom@gmail.com>
2023-02-12 15:51:30 +0700

    initial project

---
+++ src/components/FormInput.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <div>
+    <label for="name" class="label">Name {{ name }}</label>
+    <input
+      id="name"
+      type="text"
+      class="input"
+      placeholder="Enter your name"
+      v-model="test"
+    />
+    <p>ini test {{ test }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      test: "",
+    };
+  },
+};
+
+</script>
